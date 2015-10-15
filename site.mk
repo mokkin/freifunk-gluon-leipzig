357816124d16589140ca74a01dac7618fd07d771
mokkin <markus@haybach.com>
2015-10-15 23:19:40 +0200

    Sprachen und Version geändert

--- site.mk
+++ site.mk
@@ -29,13 +29,15 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_CHECKOUT := v2014.4
+DEFAULT_GLUON_CHECKOUT := v2015.1.2
 # Allow overriding the checkout from the command line
 GLUON_CHECKOUT ?= $(DEFAULT_GLUON_CHECKOUT)
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+
+GLUON_LANGS ?= de en
