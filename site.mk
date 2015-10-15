b4132b70cc670c110f12a6a4cfd50cfe8481e41e
mokkin <markus@haybach.com>
2015-10-15 23:13:55 +0200

    neue Pakete für v2015.1.2

--- site.mk
+++ site.mk
@@ -3,22 +3,27 @@ GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-authorized-keys \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-next-node \
-	gluon-neighbour-info \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
+        gluon-authorized-keys \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-core \
+        gluon-config-mode-hostname \
+        gluon-config-mode-mesh-vpn \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-contact-info \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-luci-admin \
+        gluon-luci-autoupdater \
+        gluon-luci-mesh-vpn-fastd \
+        gluon-luci-portconfig \
+        gluon-luci-private-wifi \
+        gluon-luci-wifi-config \
+        gluon-next-node \
+        gluon-neighbour-info \
+        gluon-mesh-vpn-fastd \
+        gluon-radvd \
+        gluon-status-page \
+        gluon-setup-mode \
 	iwinfo \
 	iptables \
 	haveged
